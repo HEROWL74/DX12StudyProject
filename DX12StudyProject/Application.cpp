6f67607a1406b42d4d72c84b27c7500197b02579
HEROWL <hiroakitogawa74@gmail.com>
2025-05-15 15:28:58 +0900

    5/15

--- DX12StudyProject/Application.cpp
+++ DX12StudyProject/Application.cpp
@@ -1,6 +1,6 @@
 #include <string>
 #include "Application.h"
-
+#include <vector>
 //コンストラクタ
 Application::Application(void)
 {
@@ -97,6 +97,110 @@ bool Application::SystemInit(void)
 #ifdef _DEBUG
 	else OutputDebugString("コマンドキュー5の生成 : S_OK\n");
 #endif //_DEBUG
+
+	//DXGIファクトリーの作成
+	pFactory = nullptr;
+	hr = CreateDXGIFactory1(IID_PPV_ARGS(&pFactory));
+	if (FAILED(hr)) return false;
+#ifdef _DEBUG
+	else OutputDebugString("DXGIファクトリーの作成: S_OK\n");
+#endif //_DEBUG
+
+	//スワップチェインの作成
+	DXGI_SWAP_CHAIN_DESC1 swapchainDesc = {};
+	swapchainDesc.Width = WINDOW_WID;
+	swapchainDesc.Height = WINDOW_HIG;
+	swapchainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapchainDesc.Stereo = false;
+	swapchainDesc.SampleDesc.Count = 1;
+	swapchainDesc.SampleDesc.Quality = 0;
+	swapchainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapchainDesc.BufferCount = 2;
+	//バックバッファは伸び縮み可能
+	swapchainDesc.Scaling = DXGI_SCALING_STRETCH;
+	//フリップ後はすみやかに破棄
+	swapchainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	//特に指定なし
+	swapchainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+	//ウィンドウとフルスクリーン切り替え可能
+	swapchainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+
+	hr = pFactory->CreateSwapChainForHwnd(pCmdQueue, hMainWnd, &swapchainDesc,
+		nullptr, nullptr, (IDXGISwapChain1**)&pSwapChain);
+	if (FAILED(hr)) return false;
+#ifdef _DEBUG
+	else OutputDebugString("スワップチェインの生成 : S_OK\n");
+#endif //_DEBUG
+
+	//コマンドアロケータを生成
+	hr = pDevice->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+		IID_PPV_ARGS(&pCmdAllocator));
+	if (FAILED(hr)) return false;
+#ifdef _DEBUG
+	else OutputDebugString("コマンドアロケータの生成 : S_OK\n");
+#endif //_DEBUG
+
+	hr = pDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT,
+		pCmdAllocator, nullptr, IID_PPV_ARGS(&pCmdList));
+	if (FAILED(hr)) return false;
+#ifdef _DEBUG
+	else OutputDebugString("コマンドリストの生成 : S_OK\n");
+#endif //_DEBUG
+
+	//レンダーターゲットビューの作成
+	//ディスクリプタヒープの作成
+	D3D12_DESCRIPTOR_HEAP_DESC heapDesc = {};
+	heapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV; //レンダーターゲットビューなのでRTV
+	heapDesc.NodeMask = 0;
+	heapDesc.NumDescriptors = 2;   //表裏の2つ
+	heapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;//特に指定なし
+
+	hr = pDevice->CreateDescriptorHeap(&heapDesc, IID_PPV_ARGS(&pRtvHeap));
+	if (FAILED(hr)) return false;
+#ifdef _DEBUG
+	else OutputDebugString("ディスクリプタヒープの生成 : S_OK\n");
+#endif //_DEBUG
+
+	//スワップチェインのメモリと紐づける
+	DXGI_SWAP_CHAIN_DESC swcDesc = {};
+	pSwapChain->GetDesc(&swcDesc);
+
+	D3D12_CPU_DESCRIPTOR_HANDLE handle =
+		pRtvHeap->GetCPUDescriptorHandleForHeapStart();
+
+	std::vector<ID3D12Resource*> backBuffers(swcDesc.BufferCount);
+	for (int idx = 0; idx < (int)swcDesc.BufferCount; ++idx) {
+		hr = pSwapChain->GetBuffer(idx, IID_PPV_ARGS(&backBuffers[idx]));
+		if (FAILED(hr)) return false;
+#ifdef _DEBUG
+		else OutputDebugString("GetBuffer() : S_OK\n");
+#endif //_DEBUG
+
+		pDevice->CreateRenderTargetView(backBuffers[idx], nullptr, handle);
+
+		handle.ptr += pDevice->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	}
+
+	//フェンスの作成
+	pFence = nullptr;
+	hr = pDevice->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&pFence));
+	if (FAILED(hr)) return false;
+#ifdef _DEBUG
+	else OutputDebugString("フェンスの生成 : S_OK\n");
+#endif //_DEBUG
+
+	//イベントの作成
+	fenceEvent = CreateEvent(nullptr, false, false, nullptr);
+	if (fenceEvent == nullptr)return false;
+#ifdef _DEBUG
+	else OutputDebugString("イベントの生成 : S_OK\n");
+#endif //_DEBUG
+
+	//コマンドリストを閉じる
+	pCmdList->Close();
+
+
 	return true;
 }
 
--- DX12StudyProject/main.cpp
+++ DX12StudyProject/main.cpp
@@ -1,23 +1,12 @@
 #include <Windows.h>
 #include "Application.h"
 
-int WINAPI WinMain(_In_ HINSTANCE hInstance,_In_opt_ HINSTANCE hPrevInstance,_In_ LPSTR lpCmdLine,_In_ int nCmdShow)
+int WINAPI WinMain(_In_ HINSTANCE hInst,_In_opt_ HINSTANCE hPrevInst,_In_ LPSTR lpCmdLine,_In_ int nCmdShow)
 {
-    Application app;
-
-    if (!app.Create(hInstance, nCmdShow)) {
-        MessageBox(NULL, _T("ウィンドウの作成に失敗しました。"), _T("エラー"), MB_OK | MB_ICONERROR);
-        return -1;
-    }
-
-    if (!app.SystemInit()) {
-        MessageBox(NULL, _T("システムの初期化に失敗しました。"), _T("エラー"), MB_OK | MB_ICONERROR);
-        return -1;
-    }
-
-    MSG msg = app.Run();
-
-    app.Release();
-
-    return (int)msg.wParam;
+	Application apl;
+	if (!apl.Create(hInst, nCmdShow))return -1;
+	if (!apl.SystemInit())return -1;
+	MSG msg = apl.Run();
+	return (int)msg.wParam;
 }
+
